fd1393dc57869dccb9517f885870201787faa2aa
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-02-03 22:22:52 +0530

    beta-Added nth power functionality

--- mathOperations.py
+++ mathOperations.py
@@ -1,49 +1,73 @@
 import math
+accessNaturalNumbers=[1,2,3,4,5]
+access_iter=iter(access)
 
 class mathOperations:
   def cMathOperations(inputFromUser, outputFromUser):
     inputFromUser=float(inputFromUser)
     outputFromUser=float(outputFromUser)
+    try:
+      while True:
+        if (inputFromUser**(access_iter.__next__())==outputFromUser):
+          function = 'pow(variable1, '+ str(access_iter.__next__()-1) +')'
+          return str(function)
+    except:
+      pass
     if(math.sqrt(inputFromUser)==outputFromUser):
       return 'sqrt(variable1)'
-    elif(inputFromUser**2==outputFromUser):
-      return 'pow(variable1, 2)'
     elif(math.floor(inputFromUser)==outputFromUser):
       return 'floor(variable1)'
     elif(math.ceil(inputFromUser)==outputFromUser):
       return 'ceil(variable1)'
+    else:
+      return '0'
 
   def cPlusMathOperations(inputFromUser, outputFromUser):
-    inputFromUser=int(inputFromUser)
-    outputFromUser=int(outputFromUser)
+    inputFromUser=float(inputFromUser)
+    outputFromUser=float(outputFromUser)
+    try:
+      while True:
+        if (inputFromUser**(access_iter.__next__())==outputFromUser):
+          function = 'pow(variable1, '+ str(access_iter.__next__()-1) +')'
+          return str(function)
+    except:
+      pass
     if(math.sqrt(inputFromUser)==outputFromUser):
       return 'sqrt(variable1)'
-    elif(inputFromUser**2==outputFromUser):
-      return 'pow(variable1, 2)'
     elif(math.floor(inputFromUser)==outputFromUser):
       return 'floor(variable1)'
     elif(math.ceil(inputFromUser)==outputFromUser):
       return 'ceil(variable1)'
   
   def pythonMathOperations(inputFromUser, outputFromUser):
-    inputFromUser=int(inputFromUser)
-    outputFromUser=int(outputFromUser)
+    inputFromUser=float(inputFromUser)
+    outputFromUser=float(outputFromUser)
+    try:
+      while True:
+        if (inputFromUser**(access_iter.__next__())==outputFromUser):
+          function = 'math.pow(variable1, '+ str(access_iter.__next__()-1) +')'
+          return str(function)
+    except:
+      pass
     if(math.sqrt(inputFromUser)==outputFromUser):
       return 'math.sqrt(variable1)'
-    elif(inputFromUser**2==outputFromUser):
-      return 'math.pow(variable1, 2)'
     elif(math.floor(inputFromUser)==outputFromUser):
       return 'math.floor(variable1)'
     elif(math.ceil(inputFromUser)==outputFromUser):
       return 'math.ceil(variable1)'
   
   def javaMathOperations(inputFromUser, outputFromUser):
-    inputFromUser=int(inputFromUser)
-    outputFromUser=int(outputFromUser)
+    inputFromUser=float(inputFromUser)
+    outputFromUser=float(outputFromUser)
+    try:
+      while True:
+        if (inputFromUser**(access_iter.__next__())==outputFromUser):
+          function = 'Math.pow(variable1, '+ str(access_iter.__next__()-1) +')'
+          return str(function)
+    except:
+      pass
     if(math.sqrt(inputFromUser)==outputFromUser):
       return 'Math.sqrt(variable1)'
-    elif(inputFromUser**2==outputFromUser):
-      return 'Math.pow(variable1, 2)'
     elif(math.floor(inputFromUser)==outputFromUser):
       return 'Math.floor(variable1)'
     elif(math.ceil(inputFromUser)+1==outputFromUser):
