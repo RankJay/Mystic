670aea64519a8fcf12ecc62e725a2eb50dfc36c3
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-02-03 22:11:15 +0530

    Added Ceiling and Floor functionality

--- mathOperations.py
+++ mathOperations.py
@@ -2,12 +2,16 @@ import math
 
 class mathOperations:
   def cMathOperations(inputFromUser, outputFromUser):
-    inputFromUser=int(inputFromUser)
-    outputFromUser=int(outputFromUser)
+    inputFromUser=float(inputFromUser)
+    outputFromUser=float(outputFromUser)
     if(math.sqrt(inputFromUser)==outputFromUser):
       return 'sqrt(variable1)'
     elif(inputFromUser**2==outputFromUser):
       return 'pow(variable1, 2)'
+    elif(math.floor(inputFromUser)==outputFromUser):
+      return 'floor(variable1)'
+    elif(math.ceil(inputFromUser)==outputFromUser):
+      return 'ceil(variable1)'
 
   def cPlusMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
@@ -16,6 +20,10 @@ class mathOperations:
       return 'sqrt(variable1)'
     elif(inputFromUser**2==outputFromUser):
       return 'pow(variable1, 2)'
+    elif(math.floor(inputFromUser)==outputFromUser):
+      return 'floor(variable1)'
+    elif(math.ceil(inputFromUser)==outputFromUser):
+      return 'ceil(variable1)'
   
   def pythonMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
@@ -24,6 +32,10 @@ class mathOperations:
       return 'math.sqrt(variable1)'
     elif(inputFromUser**2==outputFromUser):
       return 'math.pow(variable1, 2)'
+    elif(math.floor(inputFromUser)==outputFromUser):
+      return 'math.floor(variable1)'
+    elif(math.ceil(inputFromUser)==outputFromUser):
+      return 'math.ceil(variable1)'
   
   def javaMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
@@ -32,3 +44,7 @@ class mathOperations:
       return 'Math.sqrt(variable1)'
     elif(inputFromUser**2==outputFromUser):
       return 'Math.pow(variable1, 2)'
+    elif(math.floor(inputFromUser)==outputFromUser):
+      return 'Math.floor(variable1)'
+    elif(math.ceil(inputFromUser)+1==outputFromUser):
+      return 'Math.ceil(variable1)'
