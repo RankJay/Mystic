05fbbd2c47ad7feaea8ac22500894c86ecb24a52
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-02-03 22:00:33 +0530

    MathOperations File to compute math operations

---
+++ mathOperations.py
@@ -0,0 +1,24 @@
+class mathOperations:
+  def cMathOperations(inputFromUser, outputFromUser):
+    inputFromUser=int(inputFromUser)
+    outputFromUser=int(outputFromUser)
+    if(inputFromUser**2==outputFromUser):
+      return 'pow(variable1, 2)'
+
+  def cPlusMathOperations(inputFromUser, outputFromUser):
+    inputFromUser=int(inputFromUser)
+    outputFromUser=int(outputFromUser)
+    if(inputFromUser**2==outputFromUser):
+      return 'pow(variable1, 2)'
+  
+  def pythonMathOperations(inputFromUser, outputFromUser):
+    inputFromUser=int(inputFromUser)
+    outputFromUser=int(outputFromUser)
+    if(inputFromUser**2==outputFromUser):
+      return 'math.pow(variable1, 2)'
+  
+  def javaMathOperations(inputFromUser, outputFromUser):
+    inputFromUser=int(inputFromUser)
+    outputFromUser=int(outputFromUser)
+    if(inputFromUser**2==outputFromUser):
+      return 'Math.pow(variable1, 2)'
