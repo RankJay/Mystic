53c9e6eb2ff732b2babaff9078d6727a52fd09e8
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-02-03 22:08:24 +0530

    Added Square Root functionality

--- mathOperations.py
+++ mathOperations.py
@@ -1,24 +1,34 @@
+import math
+
 class mathOperations:
   def cMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
     outputFromUser=int(outputFromUser)
-    if(inputFromUser**2==outputFromUser):
+    if(math.sqrt(inputFromUser)==outputFromUser):
+      return 'sqrt(variable1)'
+    elif(inputFromUser**2==outputFromUser):
       return 'pow(variable1, 2)'
 
   def cPlusMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
     outputFromUser=int(outputFromUser)
-    if(inputFromUser**2==outputFromUser):
+    if(math.sqrt(inputFromUser)==outputFromUser):
+      return 'sqrt(variable1)'
+    elif(inputFromUser**2==outputFromUser):
       return 'pow(variable1, 2)'
   
   def pythonMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
     outputFromUser=int(outputFromUser)
-    if(inputFromUser**2==outputFromUser):
+    if(math.sqrt(inputFromUser)==outputFromUser):
+      return 'math.sqrt(variable1)'
+    elif(inputFromUser**2==outputFromUser):
       return 'math.pow(variable1, 2)'
   
   def javaMathOperations(inputFromUser, outputFromUser):
     inputFromUser=int(inputFromUser)
     outputFromUser=int(outputFromUser)
-    if(inputFromUser**2==outputFromUser):
+    if(math.sqrt(inputFromUser)==outputFromUser):
+      return 'Math.sqrt(variable1)'
+    elif(inputFromUser**2==outputFromUser):
       return 'Math.pow(variable1, 2)'
