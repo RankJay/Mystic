3922ba077ade66b048249d119f475efdfa8244f4
JAY RANK <53647573+RankJay@users.noreply.github.com>
2020-02-23 23:32:34 +0530

    added Data Writer file to generate a database

---
+++ deepLearning.py
@@ -0,0 +1,14 @@
+import csv
+
+class PredictionWriter:
+  def predictionDataWriter(inputFromuser, outputFromUser):
+    with open('data.csv', 'a', newline='') as deepLearningTrainer:
+      trainer = csv.writer(deepLearningTrainer)
+      try:
+        if len(inputFromuser)>1 and float(inputFromuser)==True:
+          trainer.writerow([inputFromuser, 0, outputFromUser])
+        else:
+          trainer.writerow([inputFromuser, 0, outputFromUser])
+      except ValueError:
+        if len(inputFromuser)>1:
+          trainer.writerow([inputFromuser[0], inputFromuser[2], outputFromUser])
